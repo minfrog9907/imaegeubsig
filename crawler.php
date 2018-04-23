a3b3946476f475aa2fbc4fdf6cf4d528284cd223
minfrog9907 <jimmy990791@gmail.com>
2018-04-23 10:47:23 +0900

    이매 급식친구 완성

--- crawler.php
+++ crawler.php
@@ -1,24 +1,53 @@
 <?php
+//2018-04-20 youngjun Min
     include 'simple_html_dom.php';
 
     class Crawler{
+        protected $url;
+        protected $domObject;
+
         public function Crawler($url){
-            
+           $this->url = $url;
+           $this->domObject = $this->getPage();
         }
+
         public function getLunch(){
-            
+            $lunch = $this->domObject->find('#listContent #lunch .menuName p');
+            return $this->messageCreator($lunch, "중식");
         }
 
         public function getDinner(){
-            
+            $dinner = $this->domObject->find('#listContent #dinner .menuName p');
+            return $this->messageCreator($dinner, "석식");
         }
 
-        private function messageCreator($data){
+        protected function getPage(){
+            $curl = curl_init(); 
+            curl_setopt($curl, CURLOPT_URL, $this->url);  
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);  
+            curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);  
+            $str = curl_exec($curl);  
+            curl_close($curl);  
+             
+            $html= str_get_html($str); 
+            
+            return $html;
+        }
 
+        protected function messageCreator($data, $time){
+            foreach ($data as $menu) {
+                $menu = $this->replacer($menu);
+                $message = date("Y-m-d")." 오늘의 ".$time."은 \\n\\n";
+                $message .= $menu;
+                $message .= "\\n\\n입니다. 맛있게 드세요!";
+                return $message;
+            }
+        }
+
+        protected function replacer($text){
+            $text = str_replace("<br />","\\n", $text);
+            $text = str_replace(array("!","@","#","$",".",",","1","2","3","4","5","6","7","8","9","0","<p>","</p>"," ","*"), "", $text);
+            return $text;
         }
     }
-
-    new $a = new Crawler("asdf");
-    echo($a->getLunch());
-    //commit
 ?>
--- message.php
+++ message.php
@@ -1,40 +1,41 @@
 <?php
     include 'crawler.php';
-    $crawler = new Crawler("asdf");
+    $crawler = new Crawler("http://imae.hs.kr/?act=lunch.main2&month=".date("Y.m.d")."&mcode=");
 
     $data = json_decode(file_get_contents('php://input'), true);
     $content = $data["content"];
- 
+
     switch($content)
     {
         case "중식":
-            
-        
+            $lunch = $crawler->getLunch();
+            // $lunch = "왜 안되냐... \\n 왜...";
             echo '
                 {
                     "message":
                     {
-                        "text": "중식을 선택하셨습니다."
+                        "text": "'.$lunch.'"
                     },
                     "keyboard":
-                    {
+                    {    
                         "type": "buttons",
-                        "buttons": ["메뉴1", "메뉴2", "메뉴3"]
+                        "buttons": ["중식", "석식"]
                     }
                 }';
             break;
  
         case "석식":
+            $dinner = $crawler->getDinner();
             echo '
                 {
                     "message":
                     {
-                        "text": "석식을 선택하셨습니다."
+                        "text": "'.$dinner.'"
                     },
                     "keyboard":
-                    {
+                    {    
                         "type": "buttons",
-                        "buttons": ["메뉴1", "메뉴2", "메뉴3"]
+                        "buttons": ["중식", "석식"]
                     }
                 }';
             break;
@@ -44,12 +45,12 @@
                 {
                     "message":
                     {
-                        "text": "잘못된 벨류입니다."
+                        "text": "잘못된 값입니다."
                     },
                     "keyboard":
-                    {
+                    {    
                         "type": "buttons",
-                        "buttons": ["메뉴1", "메뉴2", "메뉴3"]
+                        "buttons": ["중식", "석식"]
                     }
                 }';
             break;
